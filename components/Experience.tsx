1d5bbf4260095af224d8df709f91ba43ebd30457
Daniel K <danielkiyiki00@gmail.com>
2025-06-04 20:20:37 +0200

    final commit

---
+++ components/Experience.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="py-20" id="experience">
+      <h1 className="heading">
+        My
+        <span className="text-purple">work experience</span>
+      </h1>
+      <div className="flex-col items-center max-lg:mt-10"></div>
+    </div>
+  );
+};
+
+export default Experience;
---
+++ components/Footer.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import MagicButton from "./MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import { div } from "motion/react-client";
+import { socialMedia } from "@/data";
+
+const Footer = () => {
+  return (
+    <footer className="w-full pt-20 pb-10" id="contact">
+      <div className="w-full absolute left-0 -bottom-0 min-h-10">
+        <img src="/footer-grid.svg" alt="grid" className="w-full opacity-50" />
+      </div>
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          Ready to take <span className="text-purple">your</span> digital
+          presence to the next level?
+        </h1>
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          Reach out to me today and let&apos;s discuss how I can help you
+          achieve your goals.
+        </p>
+        <a href="mailto:danielkiyiki00@gmail.com">
+          <MagicButton
+            title="Let's get in touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2025 Daniel
+        </p>
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((profile) => (
+            <div
+              key={profile.id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img
+                src={profile.img}
+                alt={profile.id.toString()}
+                width={20}
+                height={20}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
