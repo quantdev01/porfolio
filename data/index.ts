7b0bdf584d1459ad0f7373f23b3108b608e49e50
Daniel K <danielkiyiki00@gmail.com>
2025-06-05 09:44:41 +0200

    Porfolio added some tech images

--- data/index.ts
+++ data/index.ts
@@ -97,11 +97,11 @@ export const navItems = [
     },
     {
       id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-      img: "/p4.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "https://github.com/adrianhajdin/iphone",
+      title: "Hotel Booking Site (QloApps Custom Build)",
+      des: "Custom Hotel Booking Platform with QloApps",
+      img: "/hotel.png",
+      iconLists: ["/html.svg", "/css.svg", "/php.svg", "/mysql.svg", "/apache.svg"],
+      link: "https://github.com/quantdev01/abcsuiteshotels",
     },
   ];
   
